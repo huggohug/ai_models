1f964df5788bd6e5e126abe431f169620c5745da
huggohug <166328025+huggohug@users.noreply.github.com>
2024-04-11 18:43:23 +0300

    Update provis_v2.sh

--- provis_v2.sh
+++ provis_v2.sh
@@ -28,10 +28,8 @@ CHECKPOINT_MODELS=(
     "https://civitai.com/api/download/models/176425"
     "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
     "https://civitai.com/api/download/models/15844"
-    "https://civitai.com/api/download/models/10081"
     "https://civitai.com/api/download/models/90072"
-    "https://civitai.com/api/download/models/357609"
-
+    "https://civitai.com/api/download/models/302502"
     #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
@@ -42,6 +40,7 @@ LORA_MODELS=(
     "https://civitai.com/api/download/models/44570"
     "https://civitai.com/api/download/models/73033"
     "https://civitai.com/api/download/models/30200"
+    "https://civitai.com/api/download/models/191613"
     #"https://civitai.com/api/download/models/16576"
 )
 
