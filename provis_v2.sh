4820594dc700926a9d14417f08f9ae6bd2d0cb22
huggohug <166328025+huggohug@users.noreply.github.com>
2024-04-16 19:49:55 +0300

    Update provis_v2.sh

--- provis_v2.sh
+++ provis_v2.sh
@@ -132,7 +132,7 @@ function provisioning_start() {
         "${WORKSPACE}/storage/stable_diffusion/models/upscale_models" \
         "${UPSCALE_MODELS[@]}"
     provisioning_get_models \ 
-        "${WORKSPACE}/storage/stable_diffusion/models/MotionLoRA" \
+        "${WORKSPACE}//opt/ComfyUI/custom_nodes/ComfyUI-AnimateDiff-Evolved/models/motion_lora" \
         "${MOTION_LORA[@]}"
     provisioning_print_end
 
